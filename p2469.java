ee873ba1baa59e70b0b4eb266d72803773de3e75
Srishanth <srishanth232007@gmail.com>
2026-07-28 15:17:47 +0530

    p2469 solved

---
+++ p2469.java
@@ -0,0 +1,36 @@
+// import java.util.ArrayList;
+
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        // Non-Primitive ArrayList
+        // ArrayList<Double> tempConv = new ArrayList<Double>();
+
+        // double kelvin = celsius + 273.15;
+        // tempConv.add(kelvin);
+
+        // double fahrenheit = (celsius * (9.0 / 5.0)) + 32;
+        // tempConv.add(fahrenheit);
+
+        // double[] tempConvList = new double[tempConv.size()];
+
+        // for (int i = 0; i < tempConv.size(); i++){
+        //     tempConvList[i] = tempConv.get(i);
+        // } 
+
+        // return tempConvList;
+
+
+        // Primitive Array
+        double kelvin = celsius + 273.15;
+        double fahrenheit = (celsius * (9.0 / 5.0)) + 32;
+
+        // double[] tempConv = new double[2];
+        // double[] tempConv = {kelvin, fahrenheit};
+
+        // tempConv[0] = kelvin;
+        // tempConv[1] = fahrenheit;
+
+        // return tempConv;  
+        return new double[] {kelvin, fahrenheit};
+    }
+}
