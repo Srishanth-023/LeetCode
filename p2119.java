5f962955f175e5e8c6b6ff06688fe47c75674394
Srishanth <srishanth232007@gmail.com>
2026-07-29 14:49:23 +0530

    p2119 solved

---
+++ p2119.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        // // My solution
+        // String numStr1 = Integer.toString(num);
+        // String revStr1 = new StringBuilder(numStr1).reverse().toString();
+        // int num1 = Integer.parseInt(revStr1);
+
+        // String numStr2 = Integer.toString(num1);
+        // String revStr2 = new StringBuilder(numStr2).reverse().toString();
+        // int num2 = Integer.parseInt(revStr2);
+
+        // if (num == num2){
+        //     return true;
+        // } else{
+        //     return false;
+        // }
+
+        
+
+        // General approach
+        if (num == 0){
+            return true;
+        } else if (num % 10 == 0){
+            return false;
+        }
+
+        return true;
+    }
+}
