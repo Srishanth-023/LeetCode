bc224d1342cb505cb527d3a8b3110186e937c691
Srishanth <srishanth232007@gmail.com>
2026-08-03 19:17:23 +0530

    p1470 solved (Java)

---
+++ p1470.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] shuffledArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++){
+            if (i % 2 == 0){
+                shuffledArray[i] = nums[i / 2];
+            } else{
+                shuffledArray[i] = nums[n + (i / 2)];
+            }
+        }
+
+        return shuffledArray;
+    }
+}
