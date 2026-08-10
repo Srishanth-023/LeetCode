849781b612b29655280aa3e5daa0638b4ae837aa
Srishanth <srishanth232007@gmail.com>
2026-08-10 15:20:47 +0530

    p3110 solved

---
+++ p3110.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int scoreOfString(String s) {
+        char[] charArray = s.toCharArray();
+
+        int score = 0;
+
+        for (int i = 1; i < charArray.length; i++){
+            score += Math.abs((int)charArray[i] - (int)charArray[i - 1]);
+        }
+
+        return score;
+    }
+}
